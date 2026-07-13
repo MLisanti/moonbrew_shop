113ef5e17a61345499330928f5f794482e81adba
Massimo <m_lisanti@hotmail.it>
2026-07-13 08:27:59 +0200

    Add files via upload
    puoi distinguere le pozioni quando sono una sopra l'altra, e che diavolo

---
+++ pozione.gd
@@ -0,0 +1,129 @@
+extends Area2D
+
+var _elements_scene = preload("res://scene/elemento.tscn")
+
+
+signal grab_start(node:Node2D)
+signal grab_end(node:Node2D)
+signal crea_nuova_pozione(pozione:Node2D)
+
+var _dragging = false
+const _radius_drag:int = 50
+var _grabPermission = false
+var grabIndex = 0
+
+
+var rand=RandomNumberGenerator.new()
+
+var audio1 = preload("res://suoni/glass_clink_1.mp3")
+
+var _numClientiPresi = 0
+var _clienteDaCurare:Area2D = null
+var cambiaPozione = false
+
+
+@export var price:float = 100.0
+@export var elements:String = "FA"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$helperElementi.mostra_elementi(elements, $features/Marker2D, $features)
+	$lblPrice.text = str(price) + " c"
+	
+	var nodoSprite:AnimatedSprite2D = $grafica_pozione
+	var casuale = randi_range(0, nodoSprite.sprite_frames.get_animation_names().size()-1)
+	nodoSprite.animation = nodoSprite.sprite_frames.get_animation_names()[casuale]
+
+func imposta_scala(grandezza:int):
+	match grandezza:
+		1:	$grafica_pozione.scale = Vector2(0.36, 0.36)
+		2:	$grafica_pozione.scale = Vector2(0.64, 0.64)
+		3:	$grafica_pozione.scale = Vector2(0.83, 0.83)
+		_:	$grafica_pozione.scale = Vector2(0.64, 0.64)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func audio_glass_clink():
+	var i = rand.randi_range(1, 3)
+	$audio_clink.stream = audio1
+	$audio_clink.play()
+
+func set_grab_permission(value:bool):
+	_grabPermission = value
+
+func _on_input_event(_viewport, event:InputEvent, _shape_idx):
+	
+	#touch
+	if(event is InputEventScreenTouch):
+		if((event.position - self.global_position).length() < _radius_drag):
+			grab_start.emit(self)
+			if(_grabPermission):
+				if(not _dragging and event.pressed):
+					_dragging = true
+					
+				if(_dragging and not event.pressed):
+					_dragging = false
+					grab_end.emit(self)
+	
+	#mouse
+	if(event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT):
+		if((self.global_position - event.position).length() < _radius_drag):
+			grab_start.emit(self)
+			if(_grabPermission):
+				if(not _dragging and event.pressed):
+					_dragging = true
+					
+				if(_dragging and not event.pressed):
+					_dragging = false
+					grab_end.emit(self)
+				
+	#drag
+	if(_dragging):
+		self.global_position = event.position
+	
+	#cura il cliente solo se ne hai selezionato uno e non di piu
+	if(_dragging == false and _numClientiPresi == 1 and _clienteDaCurare != null):
+		_clienteDaCurare.curaCliente(self)
+		print("effettua cura...")
+		_numClientiPresi = 0
+		_clienteDaCurare = null
+	
+	if(_dragging == false and cambiaPozione):
+		crea_nuova_pozione.emit(self)
+		
+#TODO: non si capisce bene il cliente che verrà curato ....
+
+func _on_area_entered(area):
+	if(area.is_in_group("clienti")):
+		var cliente:Area2D = area
+		cliente.mostraConfermaCura(true)
+		_numClientiPresi += 1
+		
+		#il cliente da curare è sempre l'ultimo selezionato
+		_clienteDaCurare = cliente
+		cambiaPozione = false
+	
+	print(area.name)
+	if(area.name=="farmacista"):
+		var farma = area
+		farma.mostraConfermaCambio(true)
+		
+		_clienteDaCurare = null
+		cambiaPozione = true
+
+
+func _on_area_exited(area):
+	if(area.is_in_group("clienti")):
+		var cliente:Area2D = area
+		cliente.mostraConfermaCura(false)
+		_numClientiPresi -= 1
+		
+		if(_numClientiPresi == 0):
+			_clienteDaCurare = null
+	
+	if(area.name=="farmacista"):
+		var farma = area
+		farma.mostraConfermaCambio(false)
+		cambiaPozione = false
